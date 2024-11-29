a2d335afc67f1521ed61930df70b6bafe787cf00
Mr. VannyCon <ivan.contrevida4@gmail.com>
2024-11-29 22:30:23 +0800

    add tbl_evacue_info and tbl_facilitator, add view Calamity Summary

--- calamityms_db.sql
+++ calamityms_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 18, 2024 at 02:10 PM
+-- Generation Time: Nov 29, 2024 at 03:29 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Database: `calamityms_db`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Stand-in structure for view `calamities_this_year`
+-- (See below for the actual view)
+--
+CREATE TABLE `calamities_this_year` (
+`id` int(11)
+,`calamity_active` tinyint(1)
+,`calamity_date` date
+,`calamity_time` time
+,`calamity_description` varchar(255)
+,`status_id` varchar(255)
+,`status_level` varchar(255)
+,`status_color` varchar(255)
+,`status_description` varchar(255)
+,`type_calamity_id` varchar(255)
+,`type_calamity_type` varchar(255)
+,`type_calamity_description` varchar(255)
+);
 
 -- --------------------------------------------------------
 
@@ -60,7 +81,7 @@ CREATE TABLE `tbl_announcement` (
 --
 
 INSERT INTO `tbl_announcement` (`id`, `announcement_title`, `announcement_description`, `announcement_date`, `announcement_time`) VALUES
-(6, 'Relief Goods', 'Hulat hulat lang mga maam/sir ahh ma dabot gid na sa ahh', '2024-10-18', '20:01:00');
+(9, 'Relief Goods', 'asdasdasdasdasd', '2024-11-12', '12:38:00');
 
 -- --------------------------------------------------------
 
@@ -73,6 +94,7 @@ CREATE TABLE `tbl_calamity` (
   `calamity_id` varchar(255) NOT NULL,
   `calamity_type_id` varchar(255) NOT NULL,
   `calamity_status_id` varchar(255) NOT NULL,
+  `calamity_description` varchar(255) NOT NULL,
   `calamity_active` tinyint(1) NOT NULL,
   `calamity_date` date NOT NULL,
   `calamity_time` time NOT NULL
@@ -82,14 +104,22 @@ CREATE TABLE `tbl_calamity` (
 -- Dumping data for table `tbl_calamity`
 --
 
-INSERT INTO `tbl_calamity` (`id`, `calamity_id`, `calamity_type_id`, `calamity_status_id`, `calamity_active`, `calamity_date`, `calamity_time`) VALUES
-(1, 'CALMITY-001', 'TYPE-001', 'Lvl-1', 0, '2024-10-08', '07:19:00'),
-(2, 'CALMITY-002', 'TYPE-002', 'Lvl-2', 0, '2024-10-02', '08:52:00'),
-(4, 'CALAMITY-003', 'TYPE-001', 'Lvl-1', 0, '2024-10-08', '07:19:00'),
-(5, 'CALAMITY-A8F5D19FEF', 'TYPE-004', 'Lvl-5', 0, '2024-10-17', '22:00:00'),
-(6, 'CALAMITY-BE49E1A565', 'TYPE-003', 'Lvl-2', 0, '2024-10-18', '14:12:00'),
-(7, 'CALAMITY-625ABB7B9E', 'TYPE-004', 'Lvl-2', 0, '2024-10-18', '18:10:00'),
-(8, 'CALAMITY-2B0CEBE98E', 'TYPE-001', 'Lvl-5', 1, '2024-10-18', '20:00:00');
+INSERT INTO `tbl_calamity` (`id`, `calamity_id`, `calamity_type_id`, `calamity_status_id`, `calamity_description`, `calamity_active`, `calamity_date`, `calamity_time`) VALUES
+(1, 'CALMITY-001', 'TYPE-001', 'Lvl-1', 'Bagyong Undoy', 1, '2024-10-08', '07:19:00'),
+(2, 'CALMITY-002', 'TYPE-002', 'Lvl-2', 'Bagyong Undoy', 0, '2024-10-02', '08:52:00'),
+(4, 'CALAMITY-003', 'TYPE-001', 'Lvl-1', 'Bagyong Undoy', 0, '2024-10-08', '07:19:00'),
+(5, 'CALAMITY-A8F5D19FEF', 'TYPE-004', 'Lvl-5', 'Bagyong Undoy', 0, '2024-10-17', '22:00:00'),
+(6, 'CALAMITY-BE49E1A565', 'TYPE-003', 'Lvl-2', 'Bagyong Undoy', 0, '2024-10-18', '14:12:00'),
+(7, 'CALAMITY-625ABB7B9E', 'TYPE-004', 'Lvl-2', 'Bagyong Undoy', 0, '2024-10-18', '18:10:00'),
+(8, 'CALAMITY-2B0CEBE98E', 'TYPE-001', 'Lvl-5', 'Bagyong Undoy', 0, '2024-10-18', '20:00:00'),
+(9, 'CALAMITY-A7E644B520', 'TYPE-002', 'Lvl-5', 'Bagyong Undoy', 0, '2024-10-20', '14:47:00'),
+(10, 'CALAMITY-ED18EE29C9', 'TYPE-005', 'Lvl-5', 'Bagyong Undoy', 0, '2024-10-21', '15:10:00'),
+(11, 'CALAMITY-61579C72C0', 'TYPE-001', 'Lvl-1', 'Bagyong Undoy', 0, '2024-11-12', '12:36:00'),
+(12, 'CALAMITY-2FD59C0734', 'TYPE-002', 'Lvl-4', 'Bagyong Undoy', 1, '2024-11-24', '20:05:00'),
+(13, 'CALAMITY-DF119E3B01', 'TYPE-002', 'Lvl-2', 'Magninture 2.4', 0, '2024-11-29', '21:41:00'),
+(14, 'CALAMITY-D8133C5060', 'TYPE-004', 'Lvl-3', 'Soil Problem', 0, '2024-11-27', '21:42:00'),
+(15, 'CALAMITY-E02620C3E4', 'TYPE-003', 'Lvl-5', 'High Rise of Watter', 1, '2024-11-30', '21:46:00'),
+(16, 'CALAMITY-5365A2CF4D', 'TYPE-005', 'Lvl-2', '222', 1, '2024-11-29', '21:45:00');
 
 -- --------------------------------------------------------
 
@@ -139,18 +169,18 @@ CREATE TABLE `tbl_evacuation_location` (
 
 INSERT INTO `tbl_evacuation_location` (`id`, `location_id`, `location_name`, `location_description`, `location_latitude`, `location_longhitude`, `location_current_no_of_evacuue`, `location_max_accommodate`) VALUES
 (9, 'LOCID-60AFBD759F', 'Tadlong', 'Filomino Pascual Elementary School', '10.8680771', '123.3461495', 100, 100),
-(11, 'LOCID-F6C479EDFF', 'Tadlong', 'Hautea Elementary School', '10.8481295', '123.3455575', 0, 200),
-(12, 'LOCID-27847EC0CB', 'Fabrica', 'Gil Lopez Elementary School', '10.8824647', '123.3475769', 0, 200),
-(14, 'LOCID-07C9BDD16F', 'Paraiso', 'Eusebio Lopez Memorial Integrated School', '10.8900853', '123.3540872', 0, 500),
-(15, 'LOCID-AF31286965', 'Paraiso', 'Eusebio Lopez Memorial Integrated School', '10.89024011', '123.35710957', 0, 500),
+(11, 'LOCID-F6C479EDFF', 'Tadlong', 'Hautea Elementary School', '10.8481295', '123.3455575', 100, 200),
+(12, 'LOCID-27847EC0CB', 'Fabrica', 'Gil Lopez Elementary School', '10.8824647', '123.3475769', 200, 250),
+(14, 'LOCID-07C9BDD16F', 'Paraiso', 'Eusebio Lopez Memorial Integrated School', '10.8900853', '123.3540872', 10, 500),
+(15, 'LOCID-AF31286965', 'Paraiso', 'Eusebio Lopez Memorial Integrated School', '10.89024011', '123.35710957', 10, 500),
 (16, 'LOCID-CD48F63BC1', 'Paraiso', 'Josebio Lopez Elementary School ', '10.8926998', '123.3524657', 0, 200),
 (17, 'LOCID-60AFBD759F', 'Malubon', 'Uychiat Elementary School', '10.8686271', '123.3688204', 0, 200),
 (18, 'LOCID-60AFBD759G', 'Malubon', 'Valeriana Añalucas Elementary School', '10.8692792', '123.374092', 0, 200),
-(19, 'LOCID-F6C479ED01', 'Himoga-an Baybay', 'Himogaan Baybay', '10.95533879', '123.38762770', 0, 200),
-(20, 'LOCID-F6C479ED02', 'Himogaan Baybay', 'Cesar Gamboa Elem. School', '10.9186732', '123.381358', 100, 200),
+(19, 'LOCID-F6C479ED01', 'Himoga-an Baybay', 'Himoga-an Baybay', '10.95533879', '123.38762770', 0, 200),
+(20, 'LOCID-F6C479ED02', 'Himoga-an Baybay', 'Cesar Gamboa Elem. School', '10.91980317', '123.38120563', 100, 200),
 (21, 'LOCID-F6C479ED03', 'Himoga-an Baybay', 'St. Mahilum Elementary School', '10.95410175', '123.39236322', 0, 200),
 (22, 'LOCID-70BDC879A1', 'Pob 1', 'Jose B. Puey Elementary School', '10.8935424', '123.4129442', 0, 200),
-(23, 'LOCID-70BDC879A2', 'Pob 2', 'Sagay National Highschool', '10.8916682', '123.3389246', 0, 500),
+(23, 'LOCID-70BDC879A2', 'Poblacion II (Barangay 2)', 'Sagay National Highschool', '10.88705149', '123.35101699', 0, 500),
 (24, 'LOCID-70BDC879A3', 'Pob 2', 'Ma. Lopez Elementary School', '10.894821', '123.4122776', 0, 200),
 (25, 'LOCID-70BDC879A4', 'Old Sagay', 'Old Sagay Elementary School', '10.9411558', '123.4214506', 0, 200),
 (26, 'LOCID-70BDC879A5', 'Old Sagay', 'SNHS- Old Sagay Extension', '10.932724', '123.4246615', 0, 200),
@@ -187,6 +217,51 @@ INSERT INTO `tbl_evacuation_location` (`id`, `location_id`, `location_name`, `lo
 (57, 'LOCID-70BDC879BK', 'Sewahon', 'Pacol Elementary School', '10.80676685', '123.32506052', 0, 200),
 (58, 'LOCID-70BDC879BL', 'Divina Colonia', 'Colonia Divina Integrated School', '10.7676816', '123.3075984', 0, 200),
 (59, 'LOCID-70BDC879BM', 'Plaridel', 'Plaridel Elementary School', '10.8986566', '123.4615944', 0, 200);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_evacuees_info`
+--
+
+CREATE TABLE `tbl_evacuees_info` (
+  `id` int(11) NOT NULL,
+  `fullname` varchar(255) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `age` varchar(255) NOT NULL,
+  `birthdate` varchar(255) NOT NULL,
+  `sex` varchar(255) NOT NULL,
+  `isPwd` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_evacuees_info`
+--
+
+INSERT INTO `tbl_evacuees_info` (`id`, `fullname`, `address`, `age`, `birthdate`, `sex`, `isPwd`) VALUES
+(1, 'John Dope', 'brgy.old', '24', '10/10/2024', 'Male', 1),
+(2, 'jerome natividad', 'Sagay City, Negros Occidental', '24', '2024-06-29', 'Male', 1),
+(3, 'Test Test', 'Cadiz City, Negros Occidental', '123', '2024-11-01', 'Female', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_facilitator_access`
+--
+
+CREATE TABLE `tbl_facilitator_access` (
+  `id` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_facilitator_access`
+--
+
+INSERT INTO `tbl_facilitator_access` (`id`, `username`, `password`) VALUES
+(1, 'facilitator', 'facilitator'),
+(2, 'facilitator1', 'facilitator1');
 
 -- --------------------------------------------------------
 
@@ -248,11 +323,20 @@ CREATE TABLE `view_total` (
 -- --------------------------------------------------------
 
 --
+-- Structure for view `calamities_this_year`
+--
+DROP TABLE IF EXISTS `calamities_this_year`;
+
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `calamities_this_year`  AS SELECT `c`.`id` AS `id`, `c`.`calamity_active` AS `calamity_active`, `c`.`calamity_date` AS `calamity_date`, `c`.`calamity_time` AS `calamity_time`, `c`.`calamity_description` AS `calamity_description`, `cs`.`status_id` AS `status_id`, `cs`.`status_level` AS `status_level`, `cs`.`status_color` AS `status_color`, `cs`.`status_description` AS `status_description`, `tc`.`type_calamity_id` AS `type_calamity_id`, `tc`.`type_calamity_type` AS `type_calamity_type`, `tc`.`type_calamity_description` AS `type_calamity_description` FROM ((`tbl_calamity` `c` join `tbl_calamity_status` `cs` on(`c`.`calamity_status_id` = `cs`.`status_id`)) join `tbl_type_of_calamity` `tc` on(`c`.`calamity_type_id` = `tc`.`type_calamity_id`)) WHERE `c`.`calamity_active` = 0 AND year(`c`.`calamity_date`) = year(curdate()) ORDER BY `c`.`calamity_date` DESC ;
+
+-- --------------------------------------------------------
+
+--
 -- Structure for view `view_monthly_calamity_counts`
 --
 DROP TABLE IF EXISTS `view_monthly_calamity_counts`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_monthly_calamity_counts`  AS SELECT monthname(`tbl_calamity`.`calamity_date`) AS `month_name`, count(0) AS `calamity_count` FROM `tbl_calamity` GROUP BY month(`tbl_calamity`.`calamity_date`) ORDER BY month(`tbl_calamity`.`calamity_date`) ASC ;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `view_monthly_calamity_counts`  AS SELECT monthname(`tbl_calamity`.`calamity_date`) AS `month_name`, count(0) AS `calamity_count` FROM `tbl_calamity` GROUP BY month(`tbl_calamity`.`calamity_date`) ORDER BY month(`tbl_calamity`.`calamity_date`) ASC ;
 
 -- --------------------------------------------------------
 
@@ -261,7 +345,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `view_top_calamity_type`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_top_calamity_type`  AS SELECT `tc`.`type_calamity_type` AS `calamity_type`, count(`c`.`id`) AS `calamity_count` FROM (`tbl_calamity` `c` join `tbl_type_of_calamity` `tc` on(`c`.`calamity_type_id` = `tc`.`type_calamity_id`)) WHERE year(`c`.`calamity_date`) = year(curdate()) GROUP BY `tc`.`type_calamity_type` ORDER BY count(`c`.`id`) DESC ;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `view_top_calamity_type`  AS SELECT `tc`.`type_calamity_type` AS `calamity_type`, count(`c`.`id`) AS `calamity_count` FROM (`tbl_calamity` `c` join `tbl_type_of_calamity` `tc` on(`c`.`calamity_type_id` = `tc`.`type_calamity_id`)) WHERE year(`c`.`calamity_date`) = year(curdate()) GROUP BY `tc`.`type_calamity_type` ORDER BY count(`c`.`id`) DESC ;
 
 -- --------------------------------------------------------
 
@@ -270,7 +354,7 @@ CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW 
 --
 DROP TABLE IF EXISTS `view_total`;
 
-CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `view_total`  AS SELECT (select count(`tbl_evacuation_location`.`id`) from `tbl_evacuation_location`) AS `total_evacuations`, (select count(`tbl_calamity`.`id`) from `tbl_calamity` where `tbl_calamity`.`calamity_active` = 1) AS `active_calamities` ;
+CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `view_total`  AS SELECT (select count(`tbl_evacuation_location`.`id`) from `tbl_evacuation_location`) AS `total_evacuations`, (select count(`tbl_calamity`.`id`) from `tbl_calamity` where `tbl_calamity`.`calamity_active` = 1) AS `active_calamities` ;
 
 --
 -- Indexes for dumped tables
@@ -312,6 +396,18 @@ ALTER TABLE `tbl_evacuation_location`
   ADD KEY `location_id` (`location_id`);
 
 --
+-- Indexes for table `tbl_evacuees_info`
+--
+ALTER TABLE `tbl_evacuees_info`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `tbl_facilitator_access`
+--
+ALTER TABLE `tbl_facilitator_access`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tbl_type_of_calamity`
 --
 ALTER TABLE `tbl_type_of_calamity`
@@ -332,13 +428,13 @@ ALTER TABLE `tbl_admin_access`
 -- AUTO_INCREMENT for table `tbl_announcement`
 --
 ALTER TABLE `tbl_announcement`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT for table `tbl_calamity`
 --
 ALTER TABLE `tbl_calamity`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 
 --
 -- AUTO_INCREMENT for table `tbl_calamity_status`
@@ -350,7 +446,19 @@ ALTER TABLE `tbl_calamity_status`
 -- AUTO_INCREMENT for table `tbl_evacuation_location`
 --
 ALTER TABLE `tbl_evacuation_location`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=63;
+
+--
+-- AUTO_INCREMENT for table `tbl_evacuees_info`
+--
+ALTER TABLE `tbl_evacuees_info`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `tbl_facilitator_access`
+--
+ALTER TABLE `tbl_facilitator_access`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `tbl_type_of_calamity`
